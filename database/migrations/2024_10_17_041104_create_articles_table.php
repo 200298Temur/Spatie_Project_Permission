0bf14ed3791d589372cf247c9faf4d63e4ced08d
200298Temur <temuru502@gmail.com>
2024-10-17 14:01:15 +0500

    i do

--- database/migrations/2024_10_17_041104_create_articles_table.php
+++ database/migrations/2024_10_17_041104_create_articles_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('articles', function (Blueprint $table) {
             $table->id();
             $table->string('title');
-            $table->text('text');
+            $table->text('text')->nullable();
             $table->string('author');
             $table->timestamps();
         });
